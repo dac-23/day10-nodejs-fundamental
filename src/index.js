93a7cf3c24ec9f53f23e244d24ff304966322e94
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-18 10:09:37 +0530

    basic json, its plain string.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 function main() {
-  let obj = { id: 1, name: "rohit", email: "rohit@gmail.com" };
+  // PURPOSE
+  // JSON STRING TO OBJECT LITERALS
+  let jsonStr = `{ "id": 1, "name": "rohit", "email": "rohit@gmail.com" }`;
+  let obj = JSON.parse(jsonStr);
 
-  // console.log(obj);
-  console.log(obj.id);
-  console.log(obj.name);
+  console.log(typeof jsonStr);
+  console.log(typeof obj);
+
+  // OBJECT LITERAL TO JSON STRING
+  let str1 = JSON.stringify(obj);
+  console.log(str1);
 }
 
 main();

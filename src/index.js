e3d36dd01fc104e4b89fc10fba586a1658f848df
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-18 09:42:03 +0530

    access the members. of the object.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 function main() {
-  // simplest form of object, this form is known as object literals
-  let obj = {};
-  let obj1 = { id: 1 };
-  let obj2 = { id: 1, name: "rohit" };
-  let obj3 = { id: 1, name: "rohit", email: "rohit@gmail.com" };
-  let obj4 = {
-    id: 1,
-    name: "rohit",
-    email: "rohit@gmail.com",
-    mobile: "2121212",
-  };
+  let obj = { id: 1, name: "rohit", email: "rohit@gmail.com" };
 
-  console.log(obj);
-  console.log(obj1);
-  console.log(obj2);
-  console.log(obj3);
+  // console.log(obj);
+  console.log(obj.id);
+  console.log(obj.name);
 }
 
 main();

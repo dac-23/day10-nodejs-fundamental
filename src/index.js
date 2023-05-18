03db86ee3b448eb615d15295b3a3dd0eaeca2823
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-18 10:24:08 +0530

    object literals.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 function main() {
   let obj = { id: 1, name: "rohit" };
   console.log(obj.name);
+  console.log(obj["name"]);
 
   // Modify The Member
-  obj.name = "rohit sharma";
-  obj.id = 11;
+  obj.name = "rohit sharma"; // using dot operator
+  obj["id"] = 11; // using bracket operator
   console.log(obj.name);
 
   // Add More Members
-  obj.email = "rohit@gmail.com";
-  obj.mobile = "122121";
+  obj.email = "rohit@gmail.com"; // using the dot operator
+  obj["mobile"] = "122121"; // using the bracket operator
 
   console.log(obj);
 

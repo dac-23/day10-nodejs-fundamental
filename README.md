d76cb1298440580532258a7806972d224919af49
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-18 09:36:24 +0530

    object liternals

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # day10-nodejs-fundamental
+
+// TEMPLATE :: BOUNDED
+class Person {
+// data member  
+id
+name
+email
+mobile
+}
+new Person(1, adfa, 'adfaf', 'adfa')
+
+// NO TEMPLATE :: UNBOUNDED
+let obj = {};
+let obj1 = {id:1};
+let obj2 = {id:1, name:'rohit'}
+let obj3 = {id:1, name:'rohit', email:'rohit@gmail.com'}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import {} from "node:fs";
-
 function main() {
-  console.log("Hello NodeJS!! I am modified in b1 branch");
+  // simplest form of object, this form is known as object literals
+  let obj = {};
+  let obj1 = { id: 1 };
+  let obj2 = { id: 1, name: "rohit" };
+  let obj3 = { id: 1, name: "rohit", email: "rohit@gmail.com" };
+  let obj4 = {
+    id: 1,
+    name: "rohit",
+    email: "rohit@gmail.com",
+    mobile: "2121212",
+  };
+
+  console.log(obj);
+  console.log(obj1);
+  console.log(obj2);
+  console.log(obj3);
 }
 
 main();
